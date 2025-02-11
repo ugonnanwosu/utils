e7cc20bf69706411ae3242db6382078bb0d88d8a
Ugonna Nwosu <ugonna.nwosu@rocksteadyltd.com>
2025-02-11 12:39:31 +0000

    constants - add files

---
+++ constants/exposed-defines.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object} [options={}]
+ */
+const ExposedDefines = {
+  IS_WEBPACKED,
+
+  GIT_VERSION,
+  GIT_COMMIT_HASH,
+  GIT_BRANCH,
+  GIT_LAST_COMMIT_DATETIME
+}
+
+export default ExposedDefines
