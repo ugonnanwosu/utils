d0b49e20e6af87608c3fcc6baab58794c541e6ea
Ugonna Nwosu <ugonna.nwosu@rocksteadyltd.com>
2025-02-27 13:15:32 +0000

    promised-delay.js - add files

---
+++ promise/promised-delay.js
@@ -0,0 +1,48 @@
+// libs
+
+// libs [lodash]
+import defaults from 'lodash/defaults'
+import delayFn from 'lodash/delay'
+
+// relative modules
+
+// modules
+import WrappedPromise from '@usn/utils/promise/wrapped-promise'
+
+/**
+ * @param {Function} [func]
+ * @param {number} [delay=0]
+ * @param {Object} [options={}]
+ */
+export function PromisedDelay(func, delay=0, options={}) {
+  options = defaults({ ...options }, {
+
+  });
+
+  const {
+
+  } = options;
+
+  const {
+    promise,
+    resolve,
+    reject,
+  } = WrappedPromise;
+
+  const delayId = delayFn(() => {
+    func();
+    resolve('complete');
+  }, delay);
+
+  function abort() {
+    clearTimeout(delayId);
+    reject('User aborted');
+  }
+
+  return {
+    abort,
+    promise,
+  }
+}
+
+export default PromisedDelay
