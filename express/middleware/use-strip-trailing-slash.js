a50f7af1c10fa72c7f9dfd7a540caa9885b0c941
Ugonna Nwosu <ugonna.nwosu@rocksteadyltd.com>
2025-05-17 14:37:03 +0100

    use-strip-trailing-slash.js - add file

---
+++ express/middleware/use-strip-trailing-slash.js
@@ -0,0 +1,20 @@
+// libs
+import _ from 'lodash';
+import Express from 'express'
+
+/**
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @param {Express.NextFunction} [next]
+ * @return {Promise.<string>} HTML
+ */
+function useStripTrailingSlash(req, res, next) {
+  const test = /\?[^]*\//.test(req.url);
+  if (_.last(req.url) === '/' && req.url.length > 1 && !test) {
+    res.redirect(301, req.url.slice(0, -1));
+  } else {
+    next();
+  }
+}
+
+export default useStripTrailingSlash;
