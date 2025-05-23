2cc7124e9e67b8fc6d56697c9fd550d5868ad985
Ugonna Nwosu <ugonna.nwosu@rocksteadyltd.com>
2025-05-23 10:15:40 +0100

    environment constant - add files

---
+++ constants/environment/index.js
@@ -0,0 +1,15 @@
+
+import LOCATIONS from '../environment-locations-constants';
+import STATES from '../environment-states-constants';
+
+export {
+  LOCATIONS,
+  STATES,
+}
+
+const ENVIRONMENT = {
+  LOCATIONS,
+  STATES,
+}
+
+export default ENVIRONMENT;
